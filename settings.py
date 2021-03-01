772dee7b324d1efbcabbc8f131338bd66f666c98
Totoola Kenny <40864358+Totoola-Kehinde@users.noreply.github.com>
2021-03-01 17:20:50 +0100

    simple changes

--- settings.py
+++ settings.py
@@ -1,3 +1,5 @@
+import os
+
 # Database Properties
 secret_key = '\xb3\xa2\xba\x08\xd7\xd09\x88\xc8\x9e]3"\\fi\x1c\xf3\xb8|\x9a\x01\xe9\x1d'
 MONGODB_URI = "mongodb+srv://kenny:123Kehinde#@cluster0.opvgz.mongodb.net/packages?retryWrites=true&w=majority"
